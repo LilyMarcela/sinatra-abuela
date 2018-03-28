6abafed9182be133ef4da0f4b71dadf4356a105e
Lili <marcela.b.abril@gmail.com>
2018-03-28 15:48:29 -0500

    first version

---
+++ solution.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+get '/' do
+  erb :granny
+end
+
+post '/form' do
+
+    abue = params[:message]
+
+
+    if abue == abue.upcase
+
+        <<-HTML
+        <h1> Ahh si , #{params[:message].downcase!} </h1>
+        HTML
+
+
+    else
+        <<-HTML
+        <h1> Habla mas duro mijito </h1>
+
+        HTML
+
+    end
+
+end
